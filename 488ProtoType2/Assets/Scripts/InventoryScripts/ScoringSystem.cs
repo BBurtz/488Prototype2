2e06b17018e614da08bfded4792dbf7c42c71847
BBurtz <117851173+BBurtz@users.noreply.github.com>
2025-03-24 11:18:19 -0500

    Money in top right

--- 488ProtoType2/Assets/Scripts/InventoryScripts/ScoringSystem.cs
+++ 488ProtoType2/Assets/Scripts/InventoryScripts/ScoringSystem.cs
@@ -6,6 +6,7 @@ public class ScoringSystem : InventoryHolder, IInteractable
 {
     public int score;
     public TMPro.TMP_Text ScoringText;
+    public TMPro.TMP_Text InGameText;
     public GameObject InteractPrompt;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -46,6 +47,7 @@ public class ScoringSystem : InventoryHolder, IInteractable
     private void UpdateText()
     {
         ScoringText.text = "You Made It Out With $ " + score;
+        InGameText.text = "$ " + score;
     }
 
     public void Interact(GameObject go)
